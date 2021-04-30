0981a9b9efa85f83f0dda837e33de44b39fcbee5
borramanojna <80617853+borramanojna@users.noreply.github.com>
2021-04-30 20:48:08 +0530

    Update Activity4.h

--- inc/Activity4.h
+++ inc/Activity4.h
@@ -1,6 +1,6 @@
-#ifndef ACT3_H_INCLUDED
-#define ACT3_H_INCLUDED
+#ifndef ACT4_H_INCLUDED
+#define ACT4_H_INCLUDED
 
 void InitUART(uint16_t);
 void UARTwrite(char data);
-#endif // ACT3_H_INCLUDED
+#endif // ACT4_H_INCLUDED
